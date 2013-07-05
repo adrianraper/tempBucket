4d8cc495ae54eec23c2d91e290d44c996f9cfcf1
Dave Keen <dev@ruffness.com>
2013-07-05 12:02:53 +0100

    Initial commit of TenseBusterAir

---
+++ Software/BentoTitles/TenseBusterAir/compile-dev-adhoc.bat
@@ -0,0 +1,1 @@
+"C:\Flex\4.9.1_AIR3.7\bin\adt.bat" -package -target ipa-ad-hoc -storetype pkcs12 -keystore "%USERPROFILE%\.ssh\ios-clarity\development\ios_development.p12" -storepass password -provisioning-profile "%USERPROFILE%\.ssh\ios-clarity\development\TenseBuster.mobileprovision" TenseBusterAir.ipa TenseBusterAir-app.xml TenseBusterAir.swf ios-config.xml icons
