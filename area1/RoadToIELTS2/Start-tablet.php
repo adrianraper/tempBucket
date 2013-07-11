81bb75492047e89924169ec2afa79dc7fbc7a96a
Adrian Raper <adrian.raper@clarityenglish.com>
2013-07-11 10:27:54 +0800

    Start pages

--- area1/RoadToIELTS2/Start-tablet.php
+++ area1/RoadToIELTS2/Start-tablet.php
@@ -8,17 +8,18 @@
 	// For this product
 	$productCode = '52,53'; // RoadToIELTS 2
 	$swfName = 'RoadToIELTS.swf';
-	$webShare = '';
-	$startControl = "$webShare/Software/BentoTitles/IELTS/bin-debug/";
+	$startControl = '/Software/BentoTitles/IELTS/bin-debug/';
+	$version = '1107';
+	$coordsMinWidth = '990';
+	$coordsMaxWidth = '1200';
+	$coordsMinHeight = '760';
+	$coordsMaxHeight = null;
+	$locationFile = "config.xml";
 	
 	// Picking up IP and referrer for security checking
 	require_once '../securityCheck.php';
 	
 	$locationFile = "tablet-config.xml";
-	// For PLS, IE might strip HTTP_REFERER
-	if (isset($_SESSION['Referer'])) {
-		$referrer = $_SESSION['Referer'];
-	}
 	
 	// There is a strange bug that squishes everything up if the page is empty apart from the swf
 	echo "<p/>";
@@ -33,121 +34,30 @@
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 
-	<link rel="stylesheet" type="text/css" href="<?php echo $webShare ?>/Software/Common/ielts.css" />
+	<link rel="stylesheet" type="text/css" href="/Software/Common/ielts.css" />
 
-	<script type="text/javascript" language="JavaScript" src="<?php echo $webShare ?>/Software/Common/jquery-1.7.1.min.js"></script>
-	<script type="text/javascript" language="JavaScript" src="<?php echo $webShare ?>/Software/Common/openwin.js"></script>
-	<script type="text/javascript" language="JavaScript" src="<?php echo $webShare ?>/Software/Common/swfobject2.js"></script>
-	<script type="text/javascript" language="JavaScript" src="<?php echo $webShare ?>/Software/Common/SCORM_API_Wrapper.js"></script>
+	<script type="text/javascript" language="JavaScript" src="/Software/Common/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript" language="JavaScript" src="/Software/Common/openwin.js"></script>
+	<script type="text/javascript" language="JavaScript" src="/Software/Common/swfobject2.js"></script>
 
-	<script type="text/javascript" language="JavaScript" src="<?php echo $webShare ?>/Software/Common/ielts.js"></script>
+	<script type="text/javascript" language="JavaScript" src="/Software/Common/ielts.js"></script>
+	<?php require '../phpToJavascriptVars.php'; ?>
+	<script type="text/javascript" language="JavaScript" src="/Software/Common/loadBento.js"></script>
 
+	<!-- 
+		Add any extra parameters to the flashvars array here 
+	 -->
 	<script type="text/javascript">
-		// ****
-		//
-		// ****
-		function thisMovie(movieName) {
-			if (window.document[movieName]) {
-				return window.document[movieName];
-			}
-			if (navigator.appName.indexOf("Microsoft Internet") == -1) {
-				if (document.embeds && document.embeds[movieName])
-					return document.embeds[movieName];
-			} else { // if (navigator.appName.indexOf("Microsoft Internet")!=-1)
-				return document.getElementById(movieName);
-			}
-		}
-
-		// *********
-		// *********
-		var webShare = "<?php echo $webShare ?>";
-		var startControl = "<?php echo $startControl ?>";
-		var swfName = "<?php echo $swfName ?>";
-		var versionControl = "&version=1107";
-
-		// v6.5.5.6 Allow resize screen mode
-		var coordsMinWidth = "990"; var coordsMaxWidth = "1200";
-		var coordsMinHeight = "760"; var coordsMaxHeight = null;
-
-		//var sections = location.pathname.split("/");
-		//var userdatapath = sections.slice(0,sections.length-1).join("/");
-		//var argList="?browser=true&userDataPath=" + userdatapath + "&location=<?php echo $locationFile ?>";
-		//argList+="&cache=<?php echo time() ?>";
-		
-		var argList="?configFile=<?php echo $locationFile ?>";
-		argList+="&prefix=<?php echo $prefix ?>&productCode=<?php echo $productCode ?>";
-		argList+=versionControl;
-
-		// see whether variables have come from command line or, preferentially, session variables
-		if ("<?php echo $userName ?>".length>0) {
-			var jsUserName = "<?php echo $userName ?>";
-		} else {
-			var jsUserName = swfobject.getQueryParamValue("username");
-		}
-		if ("<?php echo $password ?>".length>0) {
-			var jsPassword = "<?php echo $password ?>";
-		} else {
-			var jsPassword = swfobject.getQueryParamValue("password");
-		}
-		// If you pass studentID in command line, that seems more important than a session variable
-		var jsStudentID = swfobject.getQueryParamValue("studentID");
-		if (jsStudentID.length<=0) {
-			var jsStudentID = "<?php echo $studentID ?>";
-		}
-		if ("<?php echo $userID ?>".length>0) {
-			var jsUserID = "<?php echo $userID ?>";
-		} else {
-			var jsUserID = swfobject.getQueryParamValue("userID");
-		}
-		if ("<?php echo $Email ?>".length>0) {
-			var jsEmail = "<?php echo $Email ?>";
-		} else {
-			var jsEmail = swfobject.getQueryParamValue("email");
-		}
-		if ("<?php echo $instanceID ?>".length>0) {
-			var jsInstanceID = "<?php echo $instanceID ?>";
-		} else {
-			var jsInstanceID = swfobject.getQueryParamValue("instanceID");
-		}
-		var flashvars = {
-			username: jsUserName,
-			password: jsPassword,
-			studentID: jsStudentID,
-			userID: jsUserID,
-			email: jsEmail,
-			instanceID: jsInstanceID,
-			startingPoint: swfobject.getQueryParamValue("startingPoint"),
-			course: swfobject.getQueryParamValue("course"),
-			action: swfobject.getQueryParamValue("action"),
-			scorm: swfobject.getQueryParamValue("scorm"),
-			startTime: "<?php $msec = microtime(true); echo round(($msec * 1000)); ?>",
-			referrer: "<?php echo $referrer ?>",
-			server: "<?php echo $server ?>",
-			ip: "<?php echo $ip ?>"
-		};
-		var params = {
-			id: "bento",
-			name: "bento",
-			quality: "high",
-			allowfullscreen: "true",
-			scale: "default",
-			allowscriptaccess: "always"
-		};
-		var attr = {
-			id: "bento",
-			name: "bento"
-		};
-		var expressInstall = webShare + "/Software/Common/expressInstall.swf";
-		swfobject.embedSWF(startControl + swfName + argList, "altContent", coordsMinWidth, coordsMinHeight, "10.2.0", expressInstall, flashvars, params, attr);
+		swfobject.embedSWF(jsWebShare + jsStartControl + jsSwfName + argList, "altContent", "100%", "100%", "10.2.0", expressInstall, flashvars, params, attr);
 	</script>
 	
 <!--CSS pop up layout box-->
 <link rel="stylesheet" type="text/css" href="../../css/loadprogram.css" />
 <style type="text/css">
-	body { 	margin-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px}
+	body {margin-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px}
 </style>
 </head>
 <body onload="onLoad()">
-<?php require_once '../orchidAltContent.php';?>
+<?php require_once '../bentoAltContent.php';?>
 </body>
 </html>
