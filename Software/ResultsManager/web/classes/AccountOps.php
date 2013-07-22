dc10a7a59cd0cb65c61afd2813a8694cb11a45e9
Alice Xu <alice.xu@clarityenglish.com>
2013-07-22 12:20:48 +0800

    Fixes #403

--- Software/ResultsManager/web/classes/AccountOps.php
+++ Software/ResultsManager/web/classes/AccountOps.php
@@ -147,7 +147,8 @@ SQL;
 					case 'individuals':
 						// gh#149
 						// gh #385 Ignore for sqlite as we don't plan running DMS there
-						if (stristr($GLOBALS['dbms'],'sqlite') !== FALSE) {
+						// gh #403
+						if (stristr($GLOBALS['dbms'],'sqlite') !== 'sqlite') {
 							if ($value == 'true') {
 								//$selectBuilder->addWhere(NEG_MYPOSTFIX);
 								$selectBuilder->addWhere("a.F_Prefix REGEXP '^[0-9]+$'");
