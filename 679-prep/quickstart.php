986d45b95300016f61c5a57b1149c2e9216abd10
Adrian <adrian.raper@clarityenglish.com>
2014-01-28 11:25:11 +0800

    Google API setUp

---
+++ 679-prep/quickstart.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'google-api-php-client/src/Google_Client.php';
+require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
+
+$client = new Google_Client();
+// Get your credentials from the console
+$client->setClientId('157456417016-95juc7p1vi1h5tofe7apjv73rb94lqt3.apps.googleusercontent.com');
+$client->setClientSecret('3Zte_rcCB86bPV7sWxhR4fqt');
+$client->setRedirectUri('urn:ietf:wg:oauth:2.0:oob');
+$client->setScopes(array('https://www.googleapis.com/auth/drive'));
+
+$service = new Google_DriveService($client);
+
+$authUrl = $client->createAuthUrl();
+
+//Request authorization
+print "Please visit:\n$authUrl\n\n";
+print "Please enter the auth code:\n";
+$authCode = trim(fgets(STDIN));
+
+// Exchange authorization code for access token
+$accessToken = $client->authenticate($authCode);
+$client->setAccessToken($accessToken);
+
+//Insert a file
+$file = new Google_DriveFile();
+$file->setTitle('First upload from 679 workbench');
+$file->setDescription('A reference document');
+$file->setMimeType('text/plain');
+
+$data = file_get_contents('Reference.txt');
+
+$createdFile = $service->files->insert($file, array(
+      'data' => $data,
+      'mimeType' => 'text/plain',
+    ));
+
+print_r($createdFile);
