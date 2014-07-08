bc42e9f0720e20c9cfcd447c861bff76d59259de
Adrian <adrian.raper@clarityenglish.com>
2014-07-08 14:07:54 +0800

    Fixes #950

--- Software/ResultsManager/web/amfphp/services/createPDF.php
+++ Software/ResultsManager/web/amfphp/services/createPDF.php
@@ -1,10 +1,11 @@
 <?php
-$pdf = $GLOBALS["HTTP_RAW_POST_DATA"];
+// gh#950
+//$pdf = $GLOBALS["HTTP_RAW_POST_DATA"];
+$pdf = file_get_contents('php://input');
 header('Content-Type: application/pdf');
 header('Content-Length: '.strlen($pdf));
-header('Content-disposition:"'.$_GET["method"].'"; filename="Road to IELTS 2.pdf"');
+header('Content-disposition:"'.$_GET["method"].'"; filename="UsageStats.pdf"');
 
 echo $pdf;
 
 exit(0);
-?>
