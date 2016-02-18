1211ec144f9944acb18f88ca94ebc09bb7c021c0
Adrian <adrian.raper@clarityenglish.com>
2016-02-18 17:24:52 +0800

    Fixes #1238 print css write base node correctly and remove answers

--- Software/ResultsManager/web/amfphp/services/print.php
+++ Software/ResultsManager/web/amfphp/services/print.php
@@ -6,6 +6,8 @@ if (!isset($_GET["u"]) || !isset($_GET["b"])) exit(0);
 // Get the url and base path from the get request parameters
 $url = base64_decode($_GET["u"]);
 $basePath = base64_decode($_GET["b"]);
+//$url = "http://dock.contentbench/Content/RoadToIELTS2-International/reading/exercises/1156153794672.xml";
+//$basePath = "http://dock.contentbench/Content/RoadToIELTS2-International/reading/exercises";
 
 // Do some basic sanitization to protect against directory traversal
 // TODO: This could also match against a regular expression for URLs with an .xml suffix
@@ -16,31 +18,35 @@ $basePath = preg_replace("..", "", $basePath);
 // Remove any default namespaces before parsing anything and replace the root <bento> node with <html>
 $xmlString = file_get_contents($url);
 $xmlString = preg_replace('/( *)?xmlns[^=]*="[^"]*"/i', '', $xmlString);
-$xmlString = preg_replace("/<bento>/", "<html>", $xmlString);
-$xmlString = preg_replace("/<\/bento>/", "</html>", $xmlString);
-$simpleXml = simpledom_load_string($xmlString);
+$xmlString = preg_replace("/<bento>/i", "<html>", $xmlString);
+$xmlString = preg_replace("/<\/bento>/i", "</html>", $xmlString);
+
+// gh#1238 Some browsers will display this whilst printing, some will not. For those that do
+// change the stylesheet that is used for media="screen" to be the same as the print one
+$xmlString = preg_replace("/<link rel=\"stylesheet\" .* media=\"screen\" \/>/i", "", $xmlString);
+$xmlString = preg_replace("/media=\"print\"/i", "", $xmlString);
+
+// gh#1238 Remove the Bento model which you never want to include as it can be seen
+$xmlString = preg_replace("/<script id=\"model\"[\s\S]*<\/script>/mi", "", $xmlString);
+// Also the meta tag conversion-date
+$xmlString = preg_replace("/<meta name=\"conversion-date\".*\/>/i", "", $xmlString);
+
+// gh#1238 in php5.6 I get an error "simplexml_load_string() expects parameter 1 to be a valid callback"
+//$simpleXml = simpledom_load_string($xmlString);
+//$simpleXml = call_user_func_array('simplexml_load_string', array($xmlString, 'SimpleDOM'));
+$simpleXml = simplexml_load_string($xmlString, 'SimpleDOM');
 
 // Add in a base tag
-$baseElement = $simpleXml->head->insertBefore(new SimpleDOM('<base href="'.$basePath.'/" />'));
+// gh#1238 Make this the first child in head node
+$baseElement = new SimpleDOM('<base href="'.$basePath.'/" />');
+$firstHeadNode = $simpleXml->head->meta[0];
+$firstHeadNode->insertBeforeSelf($baseElement);
 
 // Add a copyright notice at the end of the document
-$copyrightElement = $simpleXml->body->addChild("p", "Copyright © 1993 - 2012 Clarity Language Consultants Ltd. All rights reserved.");
+// gh#1238 Change to "printed from" as we can't claim copyright of just the text
+$copyrightElement = $simpleXml->body->addChild("p", "Printed from Road to IELTS, published by British Council and ClarityEnglish.");
 $copyrightElement["id"] = "copyright";
 $copyrightElement["style"] = "border-top: 1px solid black";
-
-// Hide the content in 'screen' media mode, and replace it with a message to close the window once printing is complete
-$screenCssElement = $simpleXml->head->addChild("style",
-<<<CSS
-/*@media screen {
-	body header,
-	body section,
-	body #copyright {
-		display: none;
-	}
-}*/
-CSS
-);
-$screenCssElement["type"] = "text/css";
 
 $javascriptElement = $simpleXml->head->addChild("script",
 <<<JS
@@ -62,7 +68,8 @@ window.onload = function() {
 	}
 	
 	window.print();
-	window.close();
+	// gh#1238 Just let the user close, makes printing less dependent on friendly browser behaviour
+	//window.close();
 }
 JS
 );
