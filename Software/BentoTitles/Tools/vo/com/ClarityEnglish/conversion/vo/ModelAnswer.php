f33950f4ce4b002f4bee1b31036ebd9852058c3a
Adrian <adrian.raper@clarityenglish.com>
2017-05-18 14:52:43 +0800

    Fixes content-sss#14

--- Software/BentoTitles/Tools/vo/com/ClarityEnglish/conversion/vo/ModelAnswer.php
+++ Software/BentoTitles/Tools/vo/com/ClarityEnglish/conversion/vo/ModelAnswer.php
@@ -6,14 +6,16 @@ class ModelAnswer implements JsonSerializable {
 
     private $source = null;
     private $correct = null;
+    private $value = null;
 
     function __construct() { }
 
     function addSource($id, $prefix='a') {
         $this->source = '#'.$prefix.$id;
     }
+    // sss#14
     function addValue($text) {
-        $this->source = $text;
+        $this->value = $text;
     }
     function getSource() {
         return (isset($this->source)) ? $this->source : false;
@@ -30,6 +32,8 @@ class ModelAnswer implements JsonSerializable {
     public function jsonSerialize() {
         $rc = array();
         if (isset($this->source)) $rc['source'] = $this->source;
+        // sss#14
+        if (isset($this->value)) $rc['value'] = $this->value;
         if (isset($this->correct)) $rc['correct'] = $this->correct;
         return $rc;
     }
