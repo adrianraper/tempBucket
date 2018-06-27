ae239653270d20d61706206202469c9ea2ac85d5
Adrian <adrian.raper@clarityenglish.com>
2018-06-27 09:15:35 +0800

    dms photos

--- area1/encryptURLs.php
+++ area1/encryptURLs.php
@@ -9,7 +9,8 @@
     $urlArray = [];
     //$urlArray[] = 'prefix=TD&navigation=true&course=1287130100000&startingPoint=unit:1287130110000';
     $urlArray[] = 'prefix=Clarity&login=dandy@email&password=password';
-    
+    $urlArray[] = 'prefix=NMS&studentID=adrian';
+
     $crypt = new Crypt();
     foreach ($urlArray as $url) {
         echo "?data=".$crypt->encodeSafeChars($crypt->encrypt($url))."<br/>";
